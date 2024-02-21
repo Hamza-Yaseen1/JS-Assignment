89cbc4478a5761d02c5b1dcdc2b9cb1a67895679
Hamza-Yaseen1 <hamza.sce1@gmail.com>
2024-02-21 10:39:09 +0500

    Price calcutar

--- sec.js
+++ sec.js
@@ -1,4 +1,4 @@
 let a = prompt("Enter the amount of chicken You want to buy in gm:");
 const price = 500;
 const kg = 1000;
-document.write(price / kg * a)
+document.write("The price of ",a+"gm "," chicken is: ", price / kg * a, " rupees");
